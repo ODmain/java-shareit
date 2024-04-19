6e522ff58f3f1e861dc51f7b0a80e5469840af47
Nikita Polyakov <mr.lermontov228@yandex.ru>
2024-04-19 12:23:21 +0400

    some changes in dto's

--- src/main/java/ru/practicum/shareit/item/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/ItemDto.java
@@ -6,14 +6,19 @@ import lombok.Data;
 import ru.practicum.shareit.user.User;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
 public class ItemDto {
+    @Positive
     Long id;
     @NotEmpty
+    @Size(max = 100, min = 1, message = "Максимальная длина названия - 100 символов")
     String name;
     @NotEmpty
+    @Size(max = 255, min = 1, message = "Максимальная длина описания - 255 символов")
     String description;
     Boolean available;
     User owner;
--- src/main/java/ru/practicum/shareit/user/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -5,13 +5,17 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import static ru.practicum.shareit.constant.Constant.REGEX_EMAIL;
 
 @Data
 @Builder
 public class UserDto {
+    @Positive
     Long id;
+    @Size(max = 30, min = 1, message = "Максимальная длина имени - 30 символов")
     String name;
     @NotEmpty
     @Email(regexp = REGEX_EMAIL, message = "В 'email' использованы запрещённые символы")
